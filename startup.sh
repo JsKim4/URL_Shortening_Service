ce7a910b7fd358cd9123497f0ed76575a71a7a80
jskim4 <ggi4111@naver.com>
2021-03-13 21:12:17 +0900

    fix: 시작 스크립트 수정

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 docker-compose -f ./redis/docker-compose.yml up -d 
 docker-compose -f ./rabbitmq/docker-compose.yml up -d 
 docker-compose -f ./mysql/docker-compose.yml up -d
-nohup java -jar url_shorter\build\libs\url_shorter-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
+nohup java -jar url_shorter/build/libs/url_shorter-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
