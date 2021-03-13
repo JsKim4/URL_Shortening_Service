b06aad527d0cd103bccf8ac52e4746c5e1c230e2
jskim4 <ggi4111@naver.com>
2021-03-13 20:47:26 +0900

    fix: 실행 스크립트 수정

--- setup.sh
+++
@@ -1,3 +0,0 @@
-docker-compose -f ./redis/docker-compose.yml up -d 
-docker-compose -f ./rabbitmq/docker-compose.yml up -d 
-docker-compose -f ./mysql/docker-compose.yml up -d
--- startup.bat
+++ startup.bat
@@ -1,1 +1,4 @@
-setup.bat && nohup java -jar url_shorter\build\libs\url_shorter-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
+docker-compose -f ./redis/docker-compose.yml up -d 
+docker-compose -f ./rabbitmq/docker-compose.yml up -d 
+docker-compose -f ./mysql/docker-compose.yml up -d
+nohup java -jar url_shorter\build\libs\url_shorter-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
