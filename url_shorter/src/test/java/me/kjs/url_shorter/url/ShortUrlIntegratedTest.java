aab6783c7d64a03705e8c2f32134e5b40440b624
jskim4 <ggi4111@naver.com>
2021-03-13 20:37:36 +0900

    feat: 실행스크립트 개발

--- url_shorter/src/test/java/me/kjs/url_shorter/url/ShortUrlIntegratedTest.java
+++ url_shorter/src/test/java/me/kjs/url_shorter/url/ShortUrlIntegratedTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.testcontainers.containers.GenericContainer;
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
+@Transactional
 @Testcontainers
 @ContextConfiguration(initializers = ShortUrlIntegratedTest.ContainerPropertyInitialize.class)
 @DisplayName("URL 통합 테스트")
