28751502b792313782a6286db47a53d2e3204838
jskim4 <ggi4111@naver.com>
2021-03-14 17:57:47 +0900

    feat: startup, shutdown divide

---
+++ setdown.bat
@@ -0,0 +1,3 @@
+docker-compose -f ./redis/docker-compose.yml down 
+docker-compose -f ./rabbitmq/docker-compose.yml down
+docker-compose -f ./mysql/docker-compose.yml down
--- shutdown.bat
+++ shutdown.bat
@@ -1,4 +1,1 @@
-docker-compose -f ./redis/docker-compose.yml down 
-docker-compose -f ./rabbitmq/docker-compose.yml down
-docker-compose -f ./mysql/docker-compose.yml down
 for /f "tokens=5" %p in (' netstat -ano ^| find "LISTENING" ^| find "8080" ') do taskkill /F /PID %p
