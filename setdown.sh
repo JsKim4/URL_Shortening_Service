80240d33cd8bad9c514a6c901ca3306c15ccb44f
SeungWon <20sunrin041@sunrint.hs.kr>
2021-03-20 13:39:33 +0900

    Add Nginx

--- setdown.sh
+++ setdown.sh
@@ -1,3 +1,4 @@
 docker-compose -f ./redis/docker-compose.yml down 
 docker-compose -f ./rabbitmq/docker-compose.yml down
 docker-compose -f ./mysql/docker-compose.yml down
+docker-compose -f ./proxy/docker-compose.yml down
