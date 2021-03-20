356014cc0a536031bff5bbaf77839168b788ed55
SeungWon <20sunrin041@sunrint.hs.kr>
2021-03-20 13:39:33 +0900

    Add Nginx

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 docker-compose -f ./redis/docker-compose.yml up -d 
 docker-compose -f ./rabbitmq/docker-compose.yml up -d 
 docker-compose -f ./mysql/docker-compose.yml up -d
+docker-compose -f ./proxy/docker-compose.yml up -d
